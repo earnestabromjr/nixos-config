cf3c7e5d6d8a3bcc60f2eb0d35f829a6d7777ada
earnestabromjr <abromearnest@gmail.com>
2024-12-30 14:54:58 -0600

    New Hyprland Module

---
+++ system/WM/hypr.nix
@@ -0,0 +1,21 @@
+  {config, inputs, pkgs, ...}:
+
+{
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  environment.systemPackages = with pkgs; [
+    (waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    })
+    )
+    rofi-wayland
+    waybar
+    dunst
+    pcmanfm
+    libnotify
+    swww
+  ];
+}
