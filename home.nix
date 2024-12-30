1c3f6ae0a458c2373bd3eddf944e42918c847763
earnestabromjr <abromearnest@gmail.com>
2024-12-30 11:58:10 -0600

    Imported hyperland config

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   imports = [
     ./user/shells/sh.nix
     ./user/terminals/kitty.nix
-    ./user/WM/hyperland.nix
+    ./user/WM/hyprland.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
