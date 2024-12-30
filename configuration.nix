af3fa938731a9471d6fd93a9b9a8b2d767baf7e2
earnestabromjr <abromearnest@gmail.com>
2024-12-30 11:56:46 -0600

    Imported hyperland config

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      #./system/WM/hyprland.nix
+      system/WM/hyprland.nix
     ];
 
   options = {
--- home.nix
+++ home.nix
@@ -4,6 +4,7 @@
   imports = [
     ./user/shells/sh.nix
     ./user/terminals/kitty.nix
+    ./user/WM/hyperland.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
