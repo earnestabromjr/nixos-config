04fd3102c5bbdef8aaec7dfc5db2e2b4fa1a9bfb
earnestabromjr <abromearnest@gmail.com>
2024-12-30 15:05:09 -0600

    Added hwinfo to system pkgs

--- configuration.nix
+++ configuration.nix
@@ -127,6 +127,7 @@
     environment.systemPackages = with pkgs; [
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
+        hwinfo
     ];
 
     virtualisation.libvirtd.enable = true;
