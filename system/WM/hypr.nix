1593bb43288b5b4978d35a0ce3debc999ed01973
earnestabromjr <abromearnest@gmail.com>
2024-12-31 06:53:52 -0600

    Added hyprland specific apps to hypr.nix

--- system/WM/hypr.nix
+++ system/WM/hypr.nix
@@ -4,6 +4,7 @@
   # Hyprland
   programs.hyprland = {
     enable = true;
+    portalPackage = pkgs.xdg-desktop-portal-hyprland
     xwayland.enable = true;
   };
   environment.systemPackages = with pkgs; [
@@ -17,7 +18,17 @@
     pcmanfm
     libnotify
     swww
+    gbm
+    hyprland-protocols
+    hyprland
+    hyprutils
+    hyprwayland-scanner
     hyprland-workspaces
     hyprlandPlugins.hyprexpo
+    libdrm
+    libpipewire-0.3
+    libpsa-0.2
+    wayland-client
+    wayland-protocols
   ];
 }
