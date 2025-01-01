4ae26a0a5286da6f2bfb382c746beef6d2aff855
earnestabromjr <abromearnest@gmail.com>
2024-12-31 19:36:17 -0600

    Edit

--- user/WM/hyprland.nix
+++ user/WM/hyprland.nix
@@ -165,18 +165,6 @@
         "CTRL, 4, exec, kitty --title clock_is_kitty --hold tty-clock -C5"
         "SUPER, grave, hyprexpo:expo, toggle"
       ];
-      plugin = {
-        hyprexpo = {
-          columns = 3;
-          gap_size = 5;
-          bg_col = "rgb(111111)";
-          workspace_method = "center current";
-          enable_gesture = true;
-          gesture_fingers = 3;
-          gesture_distance = 300;
-          gesture_positive = true;
-        };
-      };
     };
   };
   gtk = {
