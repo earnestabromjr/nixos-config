52c3909192138c591e38eed6805bce73272489fb
earnestabromjr <abromearnest@gmail.com>
2025-01-10 21:27:23 -0600

    Edit

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   imports = [
     ./user/shells/sh.nix
     ./user/terminals/kitty.nix
-    ./user/WM/hyprland.nix
+    # ./user/WM/hyprland.nix
     ./user/programs.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
