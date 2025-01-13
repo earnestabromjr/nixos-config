cde54ff9a3ddbe29066a526b5fd10e656c4049ee
earnestabromjr <abromearnest@gmail.com>
2025-01-13 09:54:04 -0600

    Added gnumake to nixos config

--- configuration.nix
+++ configuration.nix
@@ -129,6 +129,7 @@
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
         hwinfo
+        gnumake
     ];
 
     virtualisation.libvirtd.enable = true;
