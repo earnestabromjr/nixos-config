88caf05f5138c9797bd42c47e915ad8f5e76f221
earnestabromjr <abromearnest@gmail.com>
2026-05-16 08:41:25 -0500

    Changed keybind in mango

--- user/WM/mangowm.nix
+++ user/WM/mangowm.nix
@@ -14,6 +14,7 @@ in
 
     autostart_sh = ''
       # mangowc autostart
+      dms run &
       swaync >/dev/null 2>&1 &
       if ! dms ipc call wallpaper get >/dev/null 2>&1; then
         swaybg -i ~/Pictures/wallpapers/wallhaven-purpleworld.jpg >/dev/null 2>&1 &
