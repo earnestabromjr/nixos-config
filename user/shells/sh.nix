9202e403ee41e1f0c1bf0a9169e8208403885538
earnestabromjr <abromearnest@gmail.com>
2026-05-26 09:49:32 -0500

    edit bump

--- user/shells/sh.nix
+++ user/shells/sh.nix
@@ -35,9 +35,8 @@
       egrep = "egrep --color=auto";
       cd = "z";
       ".." = "cd ..";
-      neovim = "nix run ~/neovim-flake# ";
+      neovim = "nix run ~/nixos-config/nixvim# ";
       homerun = "home-manager switch --flake .";
-      f = "fastfetch";
     };
   };
 }
