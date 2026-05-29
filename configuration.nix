8f34a4a8967c1dbe8cae46ff7ba4fc638bc05404
earnestabromjr <abromearnest@gmail.com>
2026-05-29 12:29:21 -0500

    added auto cpu

--- configuration.nix
+++ configuration.nix
@@ -285,7 +285,19 @@ in
   services = {
     openssh.enable = true;
     flatpak.enable = true;
-
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+        battery = {
+          governor = "powersave";
+          turbo = "auto";
+        };
+      };
+    };
   };
 
   # Nix features
