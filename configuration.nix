ba11b908b983f2a51ccf6944753ee6beaa0fc23d
earnestabromjr <abromearnest@gmail.com>
2026-05-29 12:33:33 -0500

    bump

--- configuration.nix
+++ configuration.nix
@@ -285,19 +285,6 @@ in
   services = {
     openssh.enable = true;
     flatpak.enable = true;
-    auto-cpufreq = {
-      enable = true;
-      settings = {
-        charger = {
-          governor = "performance";
-          turbo = "auto";
-        };
-        battery = {
-          governor = "powersave";
-          turbo = "auto";
-        };
-      };
-    };
   };
 
   # Nix features
